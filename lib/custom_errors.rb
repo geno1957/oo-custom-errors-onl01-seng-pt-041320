57938753edeb5ddafd2f8439981cf2656666d591
Jared  E <jared.epps322@gmail.com>
2020-05-24 02:18:06 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,7 +5,18 @@ class Person
     @name = name
   end
 
-  
+  def get_married(person)
+    self.partner = person
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+end
 
 class PartnerError < StandardError
   def message 
